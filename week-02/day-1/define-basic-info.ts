4ffd04650a026514ea63bdddabc3b6f2b31059bf
laurakocsis <laura.kocsis.95@gmail.com>
2018-10-29 11:58:50 +0100

    Tenth task define-basic-info created

---
+++ week-02/day-1/define-basic-info.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let myname: string = "Laura Kocsis";
+let age: number = 23;
+let height: number = 1.71;
+let married: boolean = false;
+
+console.log("Name: " + myname);
+console.log("Age: " + age);
+console.log("Height: " + height);
+console.log("Married: " + married);
