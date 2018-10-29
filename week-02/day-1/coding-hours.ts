340f950b1938d8fc8e698447229aef5916a1b8c5
laurakocsis <laura.kocsis.95@gmail.com>
2018-10-29 11:19:12 +0100

    Sixth task coding-hours created

---
+++ week-02/day-1/coding-hours.ts
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log("An average Green Fox attendee codes " + (17*5*6) + " hours in a semester.");
+console.log("The percentage of the coding hours is " + ((((17 * 5 * 6) / (17 * 52)) * (100)).toFixed(2)) + "%");
