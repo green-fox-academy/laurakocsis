6fdfdecefd09f1acc3d7c76d8d6f3c23079a79e3
laurakocsis <laura.kocsis.95@gmail.com>
2018-10-29 12:41:09 +0100

    Variable numbers changed according to the exercise

--- week-02/day-1/seconds-in-a-day.ts
+++ week-02/day-1/seconds-in-a-day.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-let currentHours: number = 23;
-let currentMinutes: number = 58;
-let currentSeconds: number = 30;
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
 
 console.log((60 - currentSeconds) + ((59 - currentMinutes) * 60) + ((23 - currentHours) * 60 * 60));
