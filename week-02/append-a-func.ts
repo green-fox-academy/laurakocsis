db0ab44b641337312232868c0709dbcb4aee988b
laurakocsis <laura.kocsis.95@gmail.com>
2018-10-30 10:57:17 +0100

    Task 3 - append-a-func - completed

---
+++ week-02/append-a-func.ts
@@ -0,0 +1,10 @@
+'use strict'
+export{}
+
+let typo: string = "Chinchill";
+
+function appendAFunc(input: string) {
+    let result: string = input + "a";
+    console.log(result);
+}
+appendAFunc(typo);
