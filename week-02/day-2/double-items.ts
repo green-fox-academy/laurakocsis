d0c415d9883b1dd4cc80c8d11bd4c280ef8e6dda
laurakocsis <laura.kocsis.95@gmail.com>
2018-10-30 16:09:00 +0100

    Task 15 - double-items - completed

---
+++ week-02/day-2/double-items.ts
@@ -0,0 +1,13 @@
+'use strict'
+export{}
+
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks: string [] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+let doubledrink = drinks.map (function(value, index, array) {
+    return (array [index] + array [index]);
+})
+console.log(doubledrink);
