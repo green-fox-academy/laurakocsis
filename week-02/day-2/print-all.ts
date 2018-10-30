c7049b5a459a14c322ddbeba439cf1af936cffe3
laurakocsis <laura.kocsis.95@gmail.com>
2018-10-30 14:58:37 +0100

    Task 11 - print-all - completed

---
+++ week-02/day-2/print-all.ts
@@ -0,0 +1,23 @@
+'use strict'
+export{}
+
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
+
+
+let integers = [4, 5, 6, 7];
+
+/* Solution 1
+
+for (let i: number = 0; i < integers.length; i++) {
+    console.log(integers[i]);
+}
+*/
+
+//Solution 2
+
+integers.forEach(function (e) {
+    console.log(e);
+})
