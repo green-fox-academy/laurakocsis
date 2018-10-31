fe476f3ff3be0c3e02798593e7c253c78f509502
laurakocsis <laura.kocsis.95@gmail.com>
2018-10-31 14:32:27 +0100

    Task diagonal-matrix completed

---
+++ week-02/day-2/diagonal-matrix.ts
@@ -0,0 +1,33 @@
+'use strict'
+export{}
+// -  Create (dynamically*) a two dimensional list
+//    with the following matrix**. Use a loop!
+//    0 1 2 3
+//   0 0 0 0 1
+//   1 0 0 1 0
+//   2 0 1 0 0
+//   3 1 0 0 0
+//
+// -  Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+let matrixSize: number = 4;
+
+let matrix = [];
+
+for (let i: number = 0; i < matrixSize; i++) {
+  let rowMatrix : number [] = [];
+  for (let j: number = 0; j <matrixSize; j++) {
+    if (i + j === matrixSize - 1) {
+      rowMatrix.push(1);     
+    }
+    else {
+      rowMatrix.push(0);
+    }
+  }
+  matrix.push(rowMatrix);
+  }
+
+matrix.map(e => console.log(e));
