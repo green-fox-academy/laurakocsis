73ede4f133c5da6c22f680c3c141bb3c94e5c635
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-06 11:08:46 +0100

    Task simple-replace completed - replaces a word in a string with another word

---
+++ week-03/day-2/simple-replace.ts
@@ -0,0 +1,17 @@
+'use strict';
+export {};
+
+let example: string = 'In a dishwasher far far away';
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+let wordToReplace : string = 'dishwasher';
+
+function replaceString (str: string): string {
+  let newExample = example.replace(wordToReplace, "galaxy");
+  return newExample;
+}
+
+console.log(replaceString(example));
