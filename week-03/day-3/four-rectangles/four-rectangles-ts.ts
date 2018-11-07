6469aaa6cf514b5c9a8d59cb8f4fd95b03bba4e1
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-07 11:45:55 +0100

    Task four-rectangles completed - draws 4 diff size and color rectangles

---
+++ week-03/day-3/four-rectangles/four-rectangles-ts.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// Draw four different size and color rectangles.
+// Avoid code duplication.
+
+let rectNum: number = 4;
+let colors: string [] = ['red', 'blue', 'green', 'purple'];
+
+for (let i = 0; i < rectNum; i++) {
+  drawRect(10 + i * 40, 10 + i * 30, 10 + i * 10, 10 + i * 10, colors[i])
+}
+
+function drawRect(startx, starty, x, y, color) {
+  ctx.strokeStyle = color;
+  ctx.strokeRect(startx, starty, x, y);
+}
