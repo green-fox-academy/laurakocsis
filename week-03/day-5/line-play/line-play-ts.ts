ecaf4c015b172636d706905fb8a177b4616f835d
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-09 10:24:20 +0100

    Task line-play completed

---
+++ week-03/day-5/line-play/line-play-ts.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+for (let i = 0; i < 15; i++) {
+  drawLines(100 + ((canvas.width - 100) / 15 * i), 0, canvas.width, (((canvas.height - 100) / 15) + (canvas.height - 100) / 15 * i ), 'purple');
+  drawLines(0, 100 + ((canvas.height - 100) / 15 * i), (((canvas.width - 100) / 15) + (canvas.width - 100) / 15 * i ), canvas.height, 'green');
+}
+
+function drawLines(xStart, yStart, x, y, color) {
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(xStart, yStart);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
