b210d83bfbd31a55906ff1245533f3a086da7e1c
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-09 12:30:30 +0100

    Style correction

--- week-03/day-5/line-play/line-play-ts.ts
+++ week-03/day-5/line-play/line-play-ts.ts
@@ -4,8 +4,18 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 for (let i = 0; i < 15; i++) {
-  drawLines(100 + ((canvas.width - 100) / 15 * i), 0, canvas.width, (((canvas.height - 100) / 15) + (canvas.height - 100) / 15 * i ), 'purple');
-  drawLines(0, 100 + ((canvas.height - 100) / 15 * i), (((canvas.width - 100) / 15) + (canvas.width - 100) / 15 * i ), canvas.height, 'green');
+  drawLines(
+    100 + ((canvas.width - 100) / 15 * i),
+    0,
+    canvas.width,
+    (((canvas.height - 100) / 15) + (canvas.height - 100) / 15 * i),
+    'purple');
+  drawLines(
+    0,
+    100 + ((canvas.height - 100) / 15 * i),
+    (((canvas.width - 100) / 15) + (canvas.width - 100) / 15 * i),
+    canvas.height,
+    'green');
 }
 
 function drawLines(xStart, yStart, x, y, color) {
