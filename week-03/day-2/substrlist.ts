52a66c1f3b438e653b7d9e81b7270889afde3d34
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-11 14:40:37 +0100

    Task substrlist completed - checks if a string is a substring of an element in an array

---
+++ week-03/day-2/substrlist.ts
@@ -0,0 +1,18 @@
+'use strict';
+export { }
+
+//  Create a function that takes a string and a list of string as a parameter
+//  Returns the index of the string in the list where the first string is part of
+//  Only need to find the first occurence and return the index of that
+//  Returns `-1` if the string is not part any of the strings in the list
+
+
+function substrlist(stringToSearch: string, stringList: string[]): number {
+  return stringList.findIndex(e => e.includes(stringToSearch));
+}
+
+//  Example
+console.log(substrlist('chin', ['this', 'is', 'what', 'I\'m', 'searching', 'in']));
+//  should print: `4`
+console.log(substrlist('not', ['this', 'is', 'what', 'I\'m', 'searching', 'in']));
+//  should print: `-1`
