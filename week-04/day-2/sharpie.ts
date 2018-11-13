9f5f96b2bf9a4a212d68246c8f6070f5f644c5b3
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-13 11:14:45 +0100

    Task sharpie completed - creates a class with an object and uses a method on it

---
+++ week-04/day-2/sharpie.ts
@@ -0,0 +1,21 @@
+'use strict'
+export { }
+
+class Sharpie {
+  color: string;
+  width: number;
+  inkAmount: number;
+
+  constructor(color: string, howWide: number, ink: number = 100) {
+    this.color = color;
+    this.width = howWide;
+    this.inkAmount = ink;
+  }
+  use(): void {
+    this.inkAmount -= 1;
+  }
+}
+let sharpie1 = new Sharpie('green', 99);
+sharpie1.use()
+
+console.log(sharpie1.inkAmount);
