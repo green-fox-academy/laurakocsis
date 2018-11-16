289287016535de461bdc9088fa5c14699006513d
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-16 13:55:32 +0100

    feat(triangles-fractal): parameter clean-up, animation added

--- week-04/day-5/triangles/triangles-fractal.ts
+++ week-04/day-5/triangles/triangles-fractal.ts
@@ -5,12 +5,12 @@ const ctx = canvas.getContext('2d');
 
 function drawFractal() {
   ctx.beginPath();
-  ctx.moveTo(0, 300 - 300 * Math.sqrt(3) / 2);
-  ctx.lineTo(canvas.width, 300 - 300 * Math.sqrt(3) / 2);
-  ctx.lineTo(canvas.width / 2, 300 + 300 * Math.sqrt(3) / 2);
-  ctx.lineTo(0, 300 - 300 * Math.sqrt(3) / 2);
+  ctx.moveTo(0, canvas.height / 2 - canvas.height / 2 * Math.sqrt(3) / 2);
+  ctx.lineTo(canvas.width, canvas.height / 2 - canvas.height / 2 * Math.sqrt(3) / 2);
+  ctx.lineTo(canvas.width / 2, canvas.height / 2 + canvas.height / 2 * Math.sqrt(3) / 2);
+  ctx.lineTo(0, canvas.height / 2 - canvas.height / 2 * Math.sqrt(3) / 2);
   ctx.stroke();
-  drawTriangles(150, 300, 300);
+  drawTriangles(canvas.width / 4, canvas.height / 2, 300);
 }
 
 function drawTriangles(x, y, side) {
@@ -22,9 +22,18 @@ function drawTriangles(x, y, side) {
   ctx.lineTo(x, y);
   ctx.stroke();
   if (side > 5) {
-    drawTriangles(x - side / 4, y - (side * Math.sqrt(3) / 2) / 2, side / 2);
-    drawTriangles(x + side - side / 4, y - (side * Math.sqrt(3) / 2) / 2, side / 2);
-    drawTriangles(x + side / 4, y + (side * Math.sqrt(3) / 2) / 2, side / 2);
+    setTimeout(() => {
+      drawTriangles(x - side / 4, y - (side * Math.sqrt(3) / 2) / 2, side / 2)
+    }, 600
+    );
+    setTimeout(() => {
+      drawTriangles(x + side - side / 4, y - (side * Math.sqrt(3) / 2) / 2, side / 2)
+    }, 1200
+    );
+    setTimeout(() => {
+      drawTriangles(x + side / 4, y + (side * Math.sqrt(3) / 2) / 2, side / 2)
+    }, 300
+    );
   }
 }
 
