18d8ce83a7d409e99057e5c42dac71771246162d
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-19 17:28:13 +0100

    feat(aircraft-carrrier): aircrafts class created

---
+++ week-05/day-1/aircraft-carrier/aircrafts.ts
@@ -0,0 +1,55 @@
+export class Aircrafts {
+  protected maxAmmo: number;
+  protected baseDamage: number;
+  protected ammoStore: number;
+
+  constructor() {
+    this.ammoStore = 0;
+  }
+
+  fight(): number {
+    let damage = this.ammoStore * this.baseDamage;
+    this.ammoStore = 0;
+    return damage;
+  }
+
+  refill(ammoNumber: number): number {
+    let ammoNeeded: number = this.maxAmmo - this.ammoStore;
+    if (ammoNumber >= ammoNeeded) {
+      this.ammoStore = this.maxAmmo;
+      ammoNumber -= ammoNeeded;
+    } else {
+      this.ammoStore += ammoNumber;
+      ammoNumber = 0;
+    }
+    return ammoNumber;
+  }
+
+  getType() {
+    return this.constructor.name;
+  }
+
+  getStatus(): string {
+    return (`Type ${this.getType()}, Ammo: ${this.ammoStore}, Base Damage: ${this.baseDamage}, All Damage: ${this.ammoStore * this.baseDamage}`);
+  }
+
+  isPriority(): boolean {
+    if (this.constructor.name === 'F35') {
+      return true;
+    } else if (this.constructor.name === 'F16') {
+      return false;
+    }
+  }
+
+  getmaxAmmo() {
+    return this.maxAmmo;
+  }
+
+  getbaseDamage() {
+    return this.baseDamage;
+  }
+
+  getammoStore() {
+    return this.ammoStore;
+  }
+}
