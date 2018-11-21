5b8ebec20ef295be00b1280e68e74477b4bf68cb
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-21 16:02:30 +0100

    feat(zoo): flyable interface implemented on bird class

--- week-05/day-2/zoo/bird.ts
+++ week-05/day-2/zoo/bird.ts
@@ -1,6 +1,7 @@
-import { Animal } from "./animal";
+import { Animal } from "./animal"
+import { Flyable } from "../flyable/flyable"
 
-export class Bird extends Animal {
+export class Bird extends Animal implements Flyable {
 
   constructor(name: string, age?: number, gender?: string, legNumber?: number) {
     super()
@@ -8,5 +9,17 @@ export class Bird extends Animal {
     this.age = age;
     this.gender = gender;
     this.legs = legNumber;
+  }
+
+  land() {
+    console.log(`The ${this.constructor.name} has landed.`);
+  }
+
+  fly() {
+    console.log(`The ${this.constructor.name} is flying.`);
+  }
+
+  takeoff() {
+    console.log(`The ${this.constructor.name} took off.`);
   }
 }
