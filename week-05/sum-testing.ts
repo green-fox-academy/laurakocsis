b3cf747f4bc4d26500de8946a453005ad0ba3568
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-22 11:56:24 +0100

    feat(sum): testing sum method with array of 1 element

--- week-05/sum-testing.ts
+++ week-05/sum-testing.ts
@@ -1,6 +1,7 @@
 import { test } from 'tape';
 import { Sum } from './sumclass';
 
+//Testing with multiply elements
 let numbers1 = new Sum([1, 2, 3, 4]);
 
 test('sum of numbers', t => {
@@ -8,9 +9,18 @@ test('sum of numbers', t => {
   t.end();
 });
 
+
+//Testing with empty array
 let numbers2 = new Sum([]);
 
 test('sum of numbers', t => {
   t.equal(numbers2.sum(), 10);
   t.end();
 });
+
+//Testing with a list with 1 element
+let numbers3 = new Sum([9]);
+
+test('sum of numbers', t => {
+  t.equal(numbers3.sum(), 10);
+  t.end();
