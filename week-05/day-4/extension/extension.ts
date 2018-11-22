e10582c38d25eebab53e9682ca92caf62cae5dfe
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-22 17:31:40 +0100

    feat(extension): fixed function translate

--- week-05/day-4/extension/extension.ts
+++ week-05/day-4/extension/extension.ts
@@ -16,7 +16,7 @@ export function maxOfThree(a: number, b: number, c: number): number {
 
 export function median(pool: number[]): number {
   if (pool.length % 2 === 1) {
-  return pool[(pool.length - 1) / 2];
+    return pool[(pool.length - 1) / 2];
   } else {
     return (pool[pool.length / 2] + pool[(pool.length / 2) + 1]) / 2;
   }
@@ -31,9 +31,9 @@ export function translate(hungarian: string): string {
   let length = teve.length;
 
   for (let i = 0; i < length; i++) {
-    let c = teve[i];
+    let c = teve.charAt(i);
     if (isVowel(c)) {
-      teve = teve.split(c).join(`${c}v${c}`);
+      teve = teve.substring(0, i) + `${c}v${c}` + teve.substring(i + 1, length);
       i += 2;
       length += 2;
     }
