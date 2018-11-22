75eb6ef07c5bd11e145b8a30e5636f5e07b77e5b
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-22 12:30:03 +0100

    feat(anagram): function that checks if two strings are anagrams

---
+++ week-05/day-4/anagram/anagram.ts
@@ -0,0 +1,5 @@
+export function checkAnagram(text: string, text2: string): boolean {
+  return (text.split('').sort().join() === text2.split('').sort().join());
+}
+
+console.log(checkAnagram('abcd', 'bcda'));
