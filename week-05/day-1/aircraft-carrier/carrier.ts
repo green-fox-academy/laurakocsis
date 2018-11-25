8e319352a9d6ad10557c89b0ec32b61da0c238e4
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-25 23:52:08 +0100

    feat(aircraft-carrier): completed fill method, created getStatus and totalDamage method

--- week-05/day-1/aircraft-carrier/carrier.ts
+++ week-05/day-1/aircraft-carrier/carrier.ts
@@ -12,21 +12,56 @@ export class Carrier {
     this.healthPoint = healthPoint;
   }
 
-  addAircraft(aircraft: F16 | F35) {
+  addAircraft(aircraft: F16 | F35): void {
     this.aircrafts.push(aircraft);
   }
 
-  fill() {
-    let allAmmosNeeded: number;
+  fill(): void {
+    let allAmmosNeeded: number = 0;
     this.aircrafts.forEach(e => {
-      return allAmmosNeeded += e.getammoStore()
+      allAmmosNeeded += e.getmaxAmmo();
     });
-    if (allAmmosNeeded >= this.carrierAmmoStore) {
-      this.aircrafts.forEach(e => {
-        e.refill(this.carrierAmmoStore)
-      })
-    } else if (allAmmosNeeded < this.carrierAmmoStore) {
-      
+    if (this.carrierAmmoStore === 0) {
+      throw console.log('There is no ammo in the store.');
     }
+    else {
+      if (allAmmosNeeded <= this.carrierAmmoStore) {
+        this.aircrafts.forEach(e => {
+          this.carrierAmmoStore = e.refill(this.carrierAmmoStore);
+        });
+      } else {
+        this.aircrafts.forEach(e => {
+          if (e.isPriority() && this.carrierAmmoStore > 0) {
+            this.carrierAmmoStore = e.refill(this.carrierAmmoStore);
+          }
+        });
+        this.aircrafts.forEach(e => {
+          if (!(e.isPriority()) && this.carrierAmmoStore > 0) {
+            this.carrierAmmoStore = e.refill(this.carrierAmmoStore);
+          }
+        });
+      }
+    }
+  }
+
+
+  getStatus(): void {
+    if (this.healthPoint === 0) {
+      console.log('It\'s dead Jim :(');
+    }
+    else {
+      console.log(`HP: ${this.healthPoint}, Aircraft count: ${this.aircrafts.length}, Ammo Storage: ${this.carrierAmmoStore}, Total damage: ${this.totalDamage()}`);
+      this.aircrafts.forEach(e => {
+        console.log(e.getStatus());
+      });
+    }
+  }
+
+  totalDamage(): number {
+    let totalDamage: number = 0;
+    this.aircrafts.forEach(e => {
+      totalDamage += e.getammoStore() * e.getbaseDamage();
+    });
+    return totalDamage;
   }
 }
