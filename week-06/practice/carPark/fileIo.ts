1f21e799c49b0756fbd2fcacc744a1834672b4c0
laurakocsis <laura.kocsis.95@gmail.com>
2018-11-28 22:36:17 +0100

    feat(carPark): file io created with reading and writing function

---
+++ week-06/practice/carPark/fileIo.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+export function readFromFile(fileName: string) {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  } catch (e) {
+    console.log(e.message);
+    return null;
+  }
+}
+
+export function writeToFile(fileName:string, content: string) {
+  if(fs.existsSync(fileName)) {
+    fs.writeFileSync(fileName, content)
+  } else {
+    console.log('Cannot find file to write in');
+  }
+}
+
+export function appendToFile(fileName:string, content: string) {
+  if(fs.existsSync(fileName)) {
+    fs.appendFileSync(fileName, content)
+  } else {
+    console.log('Cannot find file to write in');
+  }
+}
