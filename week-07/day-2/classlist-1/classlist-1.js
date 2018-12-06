31779a4798e5a38c55ac9aed2d78afd1d5d9493e
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-06 16:16:41 +0100

    feat(classlist1): changed the shape of the balloon background

--- week-07/day-2/classlist-1/classlist-1.js
+++ week-07/day-2/classlist-1/classlist-1.js
@@ -2,6 +2,7 @@ const paragraphs = document.querySelectorAll('p');
 
 const appleContent = document.querySelector('.apple');
 const catContent = document.querySelector('.cat');
+const balloonContent = document.querySelector('.balloon');
 
 if (paragraphs[paragraphs.length - 1].classList.contains('dolphin')) {
   appleContent.textContent = 'pear';
@@ -12,3 +13,5 @@ if (paragraphs[0].classList.contains('apple')) {
 }
 
 appleContent.classList.add('red');
+
+balloonContent.style.borderRadius = '0%';
