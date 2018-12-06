a7eea1b5b3d422a1afed3ea03f95b9847361beac
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-06 14:30:21 +0100

    feat(attribute1): exercise completed

---
+++ week-07/day-2/attribute-1/attribute-1.js
@@ -0,0 +1,10 @@
+const image = document.querySelector('img');
+console.log(image);
+
+image.setAttribute('src', 'http://4.bp.blogspot.com/-9cUoejOeQGQ/TzNi52UjzHI/AAAAAAAAAWk/r0YAdFh2Sw4/s1600/cute-bear-02.jpg');
+
+const url = document.querySelector('a');
+url.setAttribute('href', 'https://www.greenfoxacademy.com/');
+
+const secondButton = document.querySelector('.this-one');
+secondButton.setAttribute('disabled', '');
