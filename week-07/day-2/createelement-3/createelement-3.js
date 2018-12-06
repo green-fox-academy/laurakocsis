0dc2aa75e8588420684b6007bfe7650553e896b6
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-06 17:27:38 +0100

    feat(createelement3): exercise completed

---
+++ week-07/day-2/createelement-3/createelement-3.js
@@ -0,0 +1,25 @@
+const kids = [
+  { 'petName': 'Wattled crane', 'owner': 'Bryant' },
+  { 'petName': 'Devil, tasmanian', 'owner': 'Alejandro' },
+  { 'petName': 'Mynah, common', 'owner': 'Nelie' },
+  { 'petName': 'Dolphin, common', 'owner': 'Mariele' },
+  { 'petName': 'Gray duiker', 'owner': 'Maddalena' },
+  { 'petName': 'Crab (unidentified)', 'owner': 'Lucine' },
+  { 'petName': 'Ant (unidentified)', 'owner': 'Lorianna' },
+  { 'petName': 'Bison, american', 'owner': 'Tommie' },
+  { 'petName': 'Yellow mongoose', 'owner': 'Vivyan' },
+  { 'petName': 'Carpet snake', 'owner': 'Veda' },
+  { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
+];
+
+const list = document.querySelector('#pets');
+
+kids.forEach(e => {
+  const newElement = document.createElement('a');
+  const owner = document.createElement('h3');
+  const pet = document.createElement('p');
+  owner.textContent = e.owner;
+  pet.textContent = e.petName;
+  newElement.append(owner, pet);
+  list.appendChild(newElement);
+});
