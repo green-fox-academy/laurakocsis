61d91bfd83a780d99a2edad1922d07c58f463574
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-07 16:52:15 +0100

    refact(imageslider): created an array of objects for the pictures

--- week-07/day-3/imageslider.js
+++ week-07/day-3/imageslider.js
@@ -1,42 +1,79 @@
 const images = [
-  "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  "https://pbs.twimg.com/media/DMDIKcyWsAEsKDj.jpg",
-  "https://cdn.pixabay.com/photo/2016/07/02/12/21/wallpaper-1492818_960_720.jpg",
-  "https://wallpaperspit.com/wp-content/uploads/2017/11/Mountain-HD-Wallpapers-1080p-940x529.jpeg",
-  "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  "http://3.bp.blogspot.com/-0QeNuqnPXGw/T1jV6nkuU3I/AAAAAAAAUxo/G_WcVLUNIEA/s640/Free-Hd-Nature-Mountain-River-Wallpaper.jpg",
-  "http://3.bp.blogspot.com/-i1WZXxXFg1w/T1jV5DN05UI/AAAAAAAAUxE/varZtlGtEvU/s640/Beautiful-Pc-Background-Nature.jpg",
-  "http://2.bp.blogspot.com/-9vXTN8Bc0D8/T1jXtgbBU-I/AAAAAAAAU0c/yCDSOW5iC-U/s640/HD_beautiful_nature_hd_background_wallpaper.jpg"
-]
+  {
+    title: "First Title",
+    src: "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    descr: "landit lobortis dictum congue rutrum habitasse ac pretium? Integer vitae ac quis himenaeos! Ultricies potenti blandit hendrerit ultricies? Fames elementum pellentesque porttitor, massa malesuada sed proin. Magnis conubia natoque erat. Himenaeos ipsum vulputate dictumst tincidunt, massa venenatis.",
+  },
+  {
+    title: "Second Title",
+    src: "https://pbs.twimg.com/media/DMDIKcyWsAEsKDj.jpg",
+    descr: "landit lobortis dictum congue rutrum habitasse ac pretium? Integer vitae ac quis himenaeos! Ultricies potenti blandit hendrerit ultricies? Fames elementum pellentesque porttitor, massa malesuada sed proin. Magnis conubia natoque erat. Himenaeos ipsum vulputate dictumst tincidunt, massa venenatis.",
+  },
+  {
+    title: "Third Title",
+    src: "https://cdn.pixabay.com/photo/2016/07/02/12/21/wallpaper-1492818_960_720.jpg",
+    descr: "landit lobortis dictum congue rutrum habitasse ac pretium? Integer vitae ac quis himenaeos! Ultricies potenti blandit hendrerit ultricies? Fames elementum pellentesque porttitor, massa malesuada sed proin. Magnis conubia natoque erat. Himenaeos ipsum vulputate dictumst tincidunt, massa venenatis.",
+  },
+  {
+    title: "Fourth Title",
+    src: "https://wallpaperspit.com/wp-content/uploads/2017/11/Mountain-HD-Wallpapers-1080p-940x529.jpeg",
+    descr: "landit lobortis dictum congue rutrum habitasse ac pretium? Integer vitae ac quis himenaeos! Ultricies potenti blandit hendrerit ultricies? Fames elementum pellentesque porttitor, massa malesuada sed proin. Magnis conubia natoque erat. Himenaeos ipsum vulputate dictumst tincidunt, massa venenatis.",
+  },
+  {
+    title: "Fifth Title",
+    src: "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    descr: "landit lobortis dictum congue rutrum habitasse ac pretium? Integer vitae ac quis himenaeos! Ultricies potenti blandit hendrerit ultricies? Fames elementum pellentesque porttitor, massa malesuada sed proin. Magnis conubia natoque erat. Himenaeos ipsum vulputate dictumst tincidunt, massa venenatis.",
+  },
+  {
+    title: "Sixth Title",
+    src: "http://3.bp.blogspot.com/-0QeNuqnPXGw/T1jV6nkuU3I/AAAAAAAAUxo/G_WcVLUNIEA/s640/Free-Hd-Nature-Mountain-River-Wallpaper.jpg",
+    descr: "landit lobortis dictum congue rutrum habitasse ac pretium? Integer vitae ac quis himenaeos! Ultricies potenti blandit hendrerit ultricies? Fames elementum pellentesque porttitor, massa malesuada sed proin. Magnis conubia natoque erat. Himenaeos ipsum vulputate dictumst tincidunt, massa venenatis.",
+  },
+  {
+    title: "Seventh Title",
+    src: "http://3.bp.blogspot.com/-i1WZXxXFg1w/T1jV5DN05UI/AAAAAAAAUxE/varZtlGtEvU/s640/Beautiful-Pc-Background-Nature.jpg",
+    descr: "landit lobortis dictum congue rutrum habitasse ac pretium? Integer vitae ac quis himenaeos! Ultricies potenti blandit hendrerit ultricies? Fames elementum pellentesque porttitor, massa malesuada sed proin. Magnis conubia natoque erat. Himenaeos ipsum vulputate dictumst tincidunt, massa venenatis.",
+  },
+  {
+    title: "Eight Title",
+    src: "http://2.bp.blogspot.com/-9vXTN8Bc0D8/T1jXtgbBU-I/AAAAAAAAU0c/yCDSOW5iC-U/s640/HD_beautiful_nature_hd_background_wallpaper.jpg",
+    descr: "landit lobortis dictum congue rutrum habitasse ac pretium? Integer vitae ac quis himenaeos! Ultricies potenti blandit hendrerit ultricies? Fames elementum pellentesque porttitor, massa malesuada sed proin. Magnis conubia natoque erat. Himenaeos ipsum vulputate dictumst tincidunt, massa venenatis.",
+  }
+];
 
 const thumbnails = document.querySelector('.thumbnails');
 const mainImage = document.querySelector('.imageslider');
 const leftArrow = document.querySelector('button.left');
 const rightArrow = document.querySelector('button.right');
+let counter = 0;
 
-mainImage.setAttribute('src', images[0]);
+mainImage.setAttribute('src', images[0].src);
 
 for (let i = 0; i < images.length; i++) {
   const smallImg = document.createElement('img');
-  smallImg.setAttribute('src', images[i]);
+  smallImg.setAttribute('src', images[i].src);
   thumbnails.appendChild(smallImg);
 }
 
 const buttons = document.querySelectorAll('.thumbnails img');
 
 leftArrow.onclick = () => {
-  if (mainImage.getAttribute('src') === images[0]) {
-    mainImage.setAttribute('src', images[images.length - 1]);
+  if (counter === 0) {
+    counter = images.length - 1;
+    mainImage.setAttribute('src', images[counter].src);
   } else {
-    mainImage.setAttribute('src', images[images.indexOf(mainImage.getAttribute('src')) - 1]);
+    counter--;
+    mainImage.setAttribute('src', images[counter].src);
   }
 }
 
 rightArrow.onclick = () => {
-  if (mainImage.getAttribute('src') === images[images.length - 1]) {
-    mainImage.setAttribute('src', images[0]);
+  if (counter === images.length - 1) {
+    counter = 0;
+    mainImage.setAttribute('src', images[counter].src);
   } else {
-    mainImage.setAttribute('src', images[images.indexOf(mainImage.getAttribute('src')) + 1]);
+    counter++;
+    mainImage.setAttribute('src', images[counter].src);
   }
 }
 
