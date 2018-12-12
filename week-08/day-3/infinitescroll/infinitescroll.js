cb5f330494be0772929700f3f9896017b0cdeea0
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-12 17:39:39 +0100

    feat(infinitescroll): started to create scrolling function

---
+++ week-08/day-3/infinitescroll/infinitescroll.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body');
+
+console.log(window);
+
+for (let i = 0; i < 10; i++) {
+  const box = document.createElement('div');
+  box.setAttribute('class', 'box');
+  body.appendChild(box);
+  box.style.backgroundColor = 'RGB(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+}
+
+
+body.addEventListener('click', () => {
+  console.log(window.scrollY);
+  console.log(body.offsetHeight - window.innerHeight);
+});
+
+body.addEventListener('click', () => {
+  if ((body.offsetHeight - window.innerHeight) === window.scrollY) {
+    console.log("hello");
+    for (let j = 0; j < 10; j++) {
+      const box = document.createElement('div');
+      box.setAttribute('class', 'box');
+      body.appendChild(box);
+      box.style.backgroundColor = 'RGB(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+    }
+  }
+});
