ff338e3ba3514d45627d744943199b7c475a9c84
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-12 16:30:42 +0100

    feat(candygame): candy generation per seconds candy rain button added

--- week-08/day-3/candygame/game.js
+++ week-08/day-3/candygame/game.js
@@ -1,11 +1,15 @@
 const lollypop = '🍭';
 let candyCounter = 0;
 let lollypopCounter = 3;
+let candiesPerSec = 0;
+let candyRainOn = 1;
+let rainPressed = 0;
 const candies = document.querySelector('.candies');
 const lollypops = document.querySelector('.lollypops');
 const createCandy = document.querySelector('.create-candies');
 const buyLollypop = document.querySelector('.buy-lollypops');
 const candyRain = document.querySelector('.candy-machine');
+const candySpeed = document.querySelector('.speed');
 
 createCandy.addEventListener('click', () => {
   candyCounter++;
@@ -18,5 +22,24 @@ buyLollypop.addEventListener('click', () => {
     candyCounter -= 100;
     lollypops.textContent += lollypop;
     candies.textContent = candyCounter;
+  }
+});
+
+setInterval(() => {
+  if (lollypopCounter >= 10) {
+    candyCounter += Math.floor(lollypopCounter / 10) * candyRainOn;
+    candies.textContent = candyCounter;
+    candiesPerSec = Math.floor(lollypopCounter / 10) * candyRainOn;
+    candySpeed.textContent = candiesPerSec;
+  }
+}, 1000);
+
+candyRain.addEventListener('click', () => {
+  if (rainPressed === 0) {
+    rainPressed++;
+    candyRainOn = 10;
+  } else {
+    rainPressed--;
+    candyRainOn = 1;
   }
 });
