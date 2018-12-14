1899b4694a519b12bb4e8bab1ff6d8ee78d80ba1
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-14 11:16:33 +0100

    feat(infinitescroll): some christmas design added

--- week-08/day-3/infinitescroll/infinitescroll.js
+++ week-08/day-3/infinitescroll/infinitescroll.js
@@ -8,6 +8,13 @@ function createBoxes() {
     box.setAttribute('class', 'box');
     body.appendChild(box);
     box.style.backgroundColor = 'RGB(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+    const img = document.createElement('img');
+    if (i % 2 === 0) {
+      img.setAttribute('src', 'http://icons.iconarchive.com/icons/lovuhemant/merry-christmas/256/Santa-icon.png');
+    } else {
+      img.setAttribute('src', 'https://cdn.pixabay.com/photo/2016/11/30/22/10/deer-1874005_960_720.png');
+    }
+    box.appendChild(img);
   }
 }
 
