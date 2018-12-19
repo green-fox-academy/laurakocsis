11b9d2cf6b85b8af7419a5d02218accdf5c40648
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-19 16:15:55 +0100

    feat(arrayhandler): sum, multiply, double function added to endpoint

---
+++ week-09/day-3/arrayhandler.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+sum = (numArray) => {
+  let sum = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    sum += numArray[i];
+  }
+  return sum;
+}
+
+multiply = (numArray) => {
+  let multiplication = 1;
+  for (let i = 0; i < numArray.length; i++) {
+    multiplication *= numArray[i];
+  }
+  return multiplication;
+}
+
+double = (numArray) => {
+  let doubledArray = [];
+  for (let i = 0; i < numArray.length; i++) {
+    doubledArray.push(numArray[i] * 2);
+  }
+  return doubledArray;
+}
+
+
+app.post('/arrays', (req, res) => {
+  if (req.body.what === 'sum') {
+    res.json({
+      result: sum(req.body.numbers)
+    });
+  } else if (req.body.what === 'multiply') {
+    res.json({
+      result: multiply(req.body.numbers)
+    });
+  } else if (req.body.what === 'double') {
+    res.json({
+      result: double(req.body.numbers)
+    });
+  }
+});
+
+app.listen(PORT);
