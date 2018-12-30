71f069a4b12983ac9c2f435e1a36cae21b11f5c0
laurakocsis <laura.kocsis.95@gmail.com>
2018-12-30 19:04:57 +0100

    feat(starwars-api): character names are listed from 1st page based on searching

--- week-08/day-2/starwars/starwars.js
+++ week-08/day-2/starwars/starwars.js
@@ -1,17 +1,7 @@
-const characterRequest = new XMLHttpRequest();
 const filmRequest = new XMLHttpRequest();
-
-characterRequest.onreadystatechange = () => {
-  if (characterRequest.readyState === XMLHttpRequest.DONE) {
-    if (characterRequest.status === 200) {
-      const responseChar = JSON.parse(characterRequest.responseText);
-      console.log(responseChar);
-    }
-  }
-}
-
-characterRequest.open('GET', 'https://swapi.co/api/people/');
-characterRequest.send();
+const searchBtn = document.querySelector('button');
+const searchField = document.querySelector('#interactive');
+const names = document.querySelector('#characters');
 
 filmRequest.onreadystatechange = () => {
   if (filmRequest.readyState === XMLHttpRequest.DONE) {
@@ -24,3 +14,32 @@ filmRequest.onreadystatechange = () => {
 
 filmRequest.open('GET', 'https://swapi.co/api/films/');
 filmRequest.send();
+
+const nameListing = (array) => {
+  array.forEach(e => {
+    if (e.name.toLowerCase().includes(searchField.value.toLowerCase())) {
+      const a = document.createElement('a');
+      a.textContent = e.name;
+      a.setAttribute('class', 'charName');
+      names.appendChild(a);
+    }
+  });
+}
+
+searchBtn.addEventListener('click', () => {
+  let morePagesAvailable = true;
+  let pageNumber = 9;
+  const characterRequest = new XMLHttpRequest();
+  characterRequest.onload = () => {
+    if (characterRequest.status === 200) {
+      const responseChar = JSON.parse(characterRequest.responseText);
+      nameListing(responseChar.results);
+      pageNumber++;
+      if (responseChar.next === null) {
+        morePagesAvailable = false;
+      }
+    }
+  }
+  characterRequest.open('GET', `https://swapi.co/api/people/?page=${pageNumber}`);
+  characterRequest.send();
+});
