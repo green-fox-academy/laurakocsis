f97afaefb3241189a93e728dd76058e3ef8eaa94
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-02 16:02:16 +0100

    feat(starwars): movie listing function and listener added

--- week-08/day-2/starwars/starwars.js
+++ week-08/day-2/starwars/starwars.js
@@ -2,20 +2,8 @@ const filmRequest = new XMLHttpRequest();
 const searchBtn = document.querySelector('button');
 const searchField = document.querySelector('#interactive');
 const names = document.querySelector('#characters');
-const characters = document.querySelectorAll('.charName');
-const people = [];
-
-filmRequest.onreadystatechange = () => {
-  if (filmRequest.readyState === XMLHttpRequest.DONE) {
-    if (filmRequest.status === 200) {
-      const responseFilms = JSON.parse(filmRequest.responseText);
-      console.log(responseFilms);
-    }
-  }
-}
-
-filmRequest.open('GET', 'https://swapi.co/api/films/');
-filmRequest.send();
+const films = document.querySelector('#films');
+let people = [];
 
 const nameListing = (array) => {
   array.forEach(e => {
@@ -45,8 +33,42 @@ const requestResults = (url, pageNumber) => {
 }
 
 searchBtn.addEventListener('click', () => {
+  while (films.lastChild.id !== 'titles') {
+    films.removeChild(films.lastChild);
+  }
   while (names.lastChild.id !== 'names') {
     names.removeChild(names.lastChild);
   }
+  people = [];
   requestResults('https://swapi.co/api/people/?page=', 1);
+});
+
+const filmListing = (name) => {
+  people.forEach(e => {
+    if (e[0] === name) {
+      const filmRequest = new XMLHttpRequest();
+      filmRequest.onload = () => {
+        if (filmRequest.status === 200) {
+          const filmResponse = JSON.parse(filmRequest.responseText);
+          filmResponse.results.forEach(el => {
+            if (el.characters.includes(e[1])) {
+              const movie = document.createElement('p');
+              movie.textContent = el.title;
+              movie.setAttribute('class', 'movie');
+              films.appendChild(movie);
+            }
+          });
+        }
+      }
+      filmRequest.open('GET', 'https://swapi.co/api/films/');
+      filmRequest.send();
+    }
+  });
+}
+
+names.addEventListener('click', (e) => {
+  while (films.lastChild.id !== 'titles') {
+    films.removeChild(films.lastChild);
+  }
+  filmListing(e.target.innerText);
 });
