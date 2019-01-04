db4a9a3b0d2231bb6fcd7d409a83321742f6b5cc
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-04 11:49:07 +0100

    feat(bookstore): endpoint with category query added

--- week-09/day-4/bookstore/app.js
+++ week-09/day-4/bookstore/app.js
@@ -24,21 +24,37 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
   const sql =
-  `SELECT b.book_name, a.aut_name, c.cate_descrip, p.pub_name, b.book_price
+    `SELECT b.book_name, a.aut_name, c.cate_descrip, p.pub_name, b.book_price
   FROM book_mast AS b
   INNER JOIN author AS a on (b.aut_id = a.aut_id)
   INNER JOIN category AS c on (b.cate_id = c.cate_id)
   INNER JOIN publisher AS p on (b.pub_id = p.pub_id);`;
-  conn.query(sql, (err, data) => {
-    if (err) {
-      console.log(err.message);
-      res.status(500).json({
-        error: 'Internal server error'
-      });
-      return;
-    }
-    res.json(data);
-  });
+  if (!req.query.category && !req.query.publisher) {
+    conn.query(sql, (err, data) => {
+      if (err) {
+        console.log(err.message);
+        res.status(500).json({
+          error: 'Internal server error'
+        });
+        return;
+      }
+      res.json(data);
+    });
+  }
+  if (req.query.category && !req.query.publisher) {
+    const sqlCategory = sql.slice(0, sql.length - 1).concat(`WHERE c.cate_descrip = '${req.query.category}';`);
+    conn.query(sqlCategory, (err, data) => {
+      if (err) {
+        console.log(err.message);
+        res.status(500).json({
+          error: 'Internal server error'
+        });
+        return;
+      }
+      res.json(data);
+    });
+  }
 });
+
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
