d9ab1fc8c3f26bb46ab3edc3bde1f774dcdd690e
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-04 12:09:36 +0100

    refact(bookstore): function for sql query created

--- week-09/day-4/bookstore/app.js
+++ week-09/day-4/bookstore/app.js
@@ -22,65 +22,45 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));;
 });
 
+const connquery = (sql, res) => {
+  conn.query(sql, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).json({
+        error: 'Internal server error'
+      });
+      return;
+    }
+    res.json(data);
+  });
+}
+
 app.get('/books', (req, res) => {
-  const sql =
+  const sqlAll =
     `SELECT b.book_name, a.aut_name, c.cate_descrip, p.pub_name, b.book_price
   FROM book_mast AS b
   INNER JOIN author AS a on (b.aut_id = a.aut_id)
   INNER JOIN category AS c on (b.cate_id = c.cate_id)
   INNER JOIN publisher AS p on (b.pub_id = p.pub_id);`;
+
   if (!req.query.category && !req.query.publisher) {
-    conn.query(sql, (err, data) => {
-      if (err) {
-        console.log(err.message);
-        res.status(500).json({
-          error: 'Internal server error'
-        });
-        return;
-      }
-      res.json(data);
-    });
+    connquery(sqlAll, res);
   }
+
   if (req.query.category && !req.query.publisher) {
-    const sqlCategory = sql.slice(0, sql.length - 1).concat(`WHERE c.cate_descrip = '${req.query.category}';`);
-    conn.query(sqlCategory, (err, data) => {
-      if (err) {
-        console.log(err.message);
-        res.status(500).json({
-          error: 'Internal server error'
-        });
-        return;
-      }
-      res.json(data);
-    });
+    const sqlCategory = sqlAll.slice(0, sqlAll.length - 1).concat(`WHERE c.cate_descrip = '${req.query.category}';`);
+    connquery(sqlCategory, res);
   }
+
   if (!req.query.category && req.query.publisher) {
-    const sqlPublisher = sql.slice(0, sql.length - 1).concat(`WHERE p.pub_name = '${req.query.publisher}';`);
-    conn.query(sqlPublisher, (err, data) => {
-      if (err) {
-        console.log(err.message);
-        res.status(500).json({
-          error: 'Internal server error'
-        });
-        return;
-      }
-      res.json(data);
-    });
+    const sqlPublisher = sqlAll.slice(0, sqlAll.length - 1).concat(`WHERE p.pub_name = '${req.query.publisher}';`);
+    connquery(sqlPublisher, res);
   }
+
   if (req.query.category && req.query.publisher) {
-    const sqlPubCat = sql.slice(0, sql.length - 1).concat(`WHERE c.cate_descrip = '${req.query.category}' AND p.pub_name = '${req.query.publisher}';`);
-    conn.query(sqlPubCat, (err, data) => {
-      if (err) {
-        console.log(err.message);
-        res.status(500).json({
-          error: 'Internal server error'
-        });
-        return;
-      }
-      res.json(data);
-    });
+    const sqlPubCat = sqlAll.slice(0, sqlAll.length - 1).concat(`WHERE c.cate_descrip = '${req.query.category}' AND p.pub_name = '${req.query.publisher}';`);
+    connquery(sqlPubCat, res);
   }
 });
-
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
