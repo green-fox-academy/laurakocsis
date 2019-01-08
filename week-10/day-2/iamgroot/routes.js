4508041b9f474762b27f771e6b61e3fdd799bbe3
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-08 11:50:18 +0100

    feat(iamgroot): GET /groot endpoint created

---
+++ week-10/day-2/iamgroot/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/groot', (req, res) => {
+  const { message } = req.query;
+  if (message) {
+    res.status(200).json({
+      "received": `${message}`,
+      "translated": "I am Groot!"
+    });
+  } else {
+    res.status(400).json({
+      "error": "I am Groot!"
+    });
+  }
+});
+
+module.exports = app;
