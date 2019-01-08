825f8a0585b0bb0d2413f7f1174210ed932b43b7
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-08 13:43:15 +0100

    refact(arrow): data changed to number in object response

--- week-10/day-2/arrow/routes.js
+++ week-10/day-2/arrow/routes.js
@@ -5,9 +5,9 @@ app.get('/yondu', (req, res) => {
   const { distance, time } = req.query;
   if (distance && time) {
     res.status(200).json({
-      distance: `${distance}`,
-      time: `${time}`,
-      speed: `${distance / time}`
+      distance: +distance,
+      time: +time,
+      speed: distance / time
     });
   } else {
     res.status(400).json({
