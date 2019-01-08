4a7ae48458ce3acb957daf0265948acf55d8bcbc
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-08 12:22:39 +0100

    feat(yondu): /GET yondu endpoint added

---
+++ week-10/day-2/arrow/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/yondu', (req, res) => {
+  const { distance, time } = req.query;
+  if (distance && time) {
+    res.status(200).json({
+      distance: `${distance}`,
+      time: `${time}`,
+      speed: `${distance / time}`
+    });
+  } else {
+    res.status(400).json({
+      error: `Distance or time is missing`
+    });
+  }
+});
+
+module.exports = app;
