42ce1df995175e766583f91f4dd310e874b975c7
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-08 20:21:38 +0100

    feat(quizapp): GET /gamme endpoint added

---
+++ week-10/day-4/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'game.html'));
+});
+
+app.get('/manage', (req, res) => {
+  res.sendFile(path.join(__dirname, '/static/manage.html'));
+});
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'quizapp'
+});
+
+app.get('/game', (req, res) => {
+  conn.query('SELECT * FROM questions;', (err, questions) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).json({
+        error: 'Internal server error'
+      });
+      return;
+    }
+    const questionNumber = Math.max.apply(Math, questions.map(function (ids) { return ids.id; }));
+    const randomId = Math.floor(Math.random() * questionNumber) + 1;
+    conn.query(`SELECT * FROM answers WHERE question_id=${randomId};`, (e, answers) => {
+      if (err) {
+        console.log(e.message);
+        res.status(500).json({
+          error: 'Internal server error'
+        });
+        return;
+      }
+      res.json({
+        id: questions[randomId - 1].id,
+        question: questions[randomId - 1].question,
+        answers: answers
+      });
+    });
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
