93c75fc1c17ed0b07b989d485c38ef7c9ffd9435
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-09 17:32:09 +0100

    feat(quizapp): quiz app added with score counter

---
+++ week-10/day-4/static/game.js
@@ -0,0 +1,45 @@
+const question = document.querySelector('h2');
+const score = document.querySelector('span');
+let points = 0;
+const options = document.querySelectorAll('button');
+const answer = document.querySelector('#questions');
+
+const createQuestion = (data) => {
+  question.innerText = data.question;
+  options.forEach((e, i) => {
+    e.innerText = data.answers[i].answer;
+    e.setAttribute('value', data.answers[i].is_correct);
+    e.style.background = '#3498db';
+  });
+}
+
+const questionRequest = (callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  }
+  xhr.open('GET', '/game');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send();
+} 
+
+questionRequest(createQuestion);
+
+answer.addEventListener('click', (event) => {
+  const { name, value, id } = event.target;
+  const chosenAnswer = document.querySelector(`#${id}`);
+  if (name === 'answer') {
+    if (value === '1') {
+      chosenAnswer.style.background = '#4ac100';
+      points++;
+      score.innerText = points;
+    } else {
+      chosenAnswer.style.background = '#ff5454';
+    }
+    setTimeout(() => {
+      questionRequest(createQuestion);
+    }, 2000);
+  }
+}); 
