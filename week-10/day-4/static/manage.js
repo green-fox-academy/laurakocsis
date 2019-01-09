590de8a1834aa8e191311d6feb77327cf99ef8ad
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-09 21:08:07 +0100

    feat(quizapp): manage add questions on frontend

--- week-10/day-4/static/manage.js
+++ week-10/day-4/static/manage.js
@@ -1,6 +1,11 @@
 const questions = document.querySelector('#questions');
+const form = document.querySelector('form');
+const radiobuttons = document.querySelectorAll('input[type=radio]');
 
 const appendQuestions = (dataArray) => {
+  while (questions.firstChild) {
+    questions.removeChild(questions.firstChild);
+  }
   dataArray.forEach(e => {
     const div = document.createElement('div');
     div.setAttribute('class', 'question');
@@ -27,3 +32,36 @@ const questionsRequest = (callback) => {
 }
 
 questionsRequest(appendQuestions);
+
+const sendNewQuestion = () => {
+  const { newQuestion, answerone, answertwo, answerthree, answerfour, correctanswer } = form.elements;
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/questions');
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      questionsRequest(appendQuestions);
+      newQuestion.value = '';
+      answerone.value = '';
+      answertwo.value = '';
+      answerthree.value = '';
+      answerfour.value = '';
+      radiobuttons.forEach(e => {
+        e.checked = false;
+      });
+    }
+  }
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    question: newQuestion.value,
+    answer1: answerone.value,
+    answer2: answertwo.value,
+    answer3: answerthree.value,
+    answer4: answerfour.value,
+    correct: correctanswer.value
+  }));
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendNewQuestion();
+});
